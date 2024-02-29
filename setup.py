3e274e85edb184a9092bc0dcb8f5be7b1798d190
root <root@vmi1533510.contaboserver.net>
2024-03-01 06:49:12 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -15,8 +15,15 @@ setup(
     author='cann',
     author_email='savemefromthedark777@gmail.com',
     description='A calculator application by Cann',
-    url='https://github.com/cann072000/cann_calculator',
-    license='MIT',
+    bugtrack_url='https://github.com/cann072000/cann_calculator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.8',
+    ],
     project_urls={
         'Source': 'https://github.com/cann072000/cann_calculator',
     },
